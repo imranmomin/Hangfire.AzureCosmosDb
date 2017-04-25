a3aed6915883e4018a899b5f8e19428df0f7b2f1
imranmomin <imran.amomin@outlook.com>
2017-04-25 09:03:38 -0400

    removed the extension

--- Hangfire.AzureDocumentDB/Entities/State.cs
+++ Hangfire.AzureDocumentDB/Entities/State.cs
@@ -24,17 +24,4 @@ namespace Hangfire.AzureDocumentDB.Entities
         [JsonProperty("data")]
         public Dictionary<string, string> Data { get; set; }
     }
-
-    internal static class StateExtension
-    {
-        internal static Dictionary<string, string> Trasnform(this Dictionary<string, string> data) => data.ToDictionary(k => k.Key.ToProperCase(), v => v.Value);
-
-        private static string ToProperCase(this string key)
-        {
-            string[] keys = key.Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries)
-                               .Select(k => System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(k))
-                               .ToArray();
-            return string.Join(string.Empty, keys);
-        }
-    }
 }
