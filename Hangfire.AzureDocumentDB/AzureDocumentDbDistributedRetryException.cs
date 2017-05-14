213d0a4842f332c1fc354ab3792a0b556eadfa0f
Imran Momin <imran.amomin@outlook.com>
2017-05-14 09:34:43 -0400

    helper class to execute with retires

---
+++ Hangfire.AzureDocumentDB/AzureDocumentDbDistributedRetryException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hangfire.AzureDocumentDB
+{
+    /// <summary>
+    /// Represents errors that occur while acquiring a distributed lock.
+    /// </summary>
+    [Serializable]
+    public class AzureDocumentDbDistributedRetryException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the AzureDocumentDbDistributedRetryException class with serialized data.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public AzureDocumentDbDistributedRetryException(string message) : base(message)
+        {
+        }
+    }
+}
