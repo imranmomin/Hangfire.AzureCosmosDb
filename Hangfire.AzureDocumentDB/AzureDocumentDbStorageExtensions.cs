4913789f068e210dc50f2dabb2ce6a02c4f80c03
imranmomin <imran.amomin@outlook.com>
2017-05-26 16:22:42 -0400

    UseAzureDocumentDbStorage support for collection name

--- Hangfire.AzureDocumentDB/AzureDocumentDbStorageExtensions.cs
+++ Hangfire.AzureDocumentDB/AzureDocumentDbStorageExtensions.cs
@@ -15,14 +15,15 @@ namespace Hangfire
         /// <param name="url">The url string to AzureDocumentDb Database</param>
         /// <param name="authSecret">The secret key for the AzureDocumentDb Database</param>
         /// <param name="database">The name of the database to connect with</param>
+        /// <param name="collection">The name of the collection on the database</param>
         /// <returns></returns>
-        public static IGlobalConfiguration<AzureDocumentDbStorage> UseAzureDocumentDbStorage(this IGlobalConfiguration configuration, string url, string authSecret, string database)
+        public static IGlobalConfiguration<AzureDocumentDbStorage> UseAzureDocumentDbStorage(this IGlobalConfiguration configuration, string url, string authSecret, string database, string collection)
         {
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
             if (string.IsNullOrEmpty(url)) throw new ArgumentNullException(nameof(url));
             if (string.IsNullOrEmpty(authSecret)) throw new ArgumentNullException(nameof(authSecret));
 
-            AzureDocumentDbStorage storage = new AzureDocumentDbStorage(url, authSecret, database);
+            AzureDocumentDbStorage storage = new AzureDocumentDbStorage(url, authSecret, database, collection);
             return configuration.UseStorage(storage);
         }
 
@@ -33,16 +34,17 @@ namespace Hangfire
         /// <param name="url">The url string to AzureDocumentDb Database</param>
         /// <param name="authSecret">The secret key for the AzureDocumentDb Database</param>
         /// <param name="database">The name of the database to connect with</param>
+        /// <param name="collection">The name of the collection on the database</param>
         /// <param name="options">The AzureDocumentDbStorage object to override any of the options</param>
         /// <returns></returns>
-        public static IGlobalConfiguration<AzureDocumentDbStorage> UseAzureDocumentDbStorage(this IGlobalConfiguration configuration, string url, string authSecret, string database, AzureDocumentDbStorageOptions options)
+        public static IGlobalConfiguration<AzureDocumentDbStorage> UseAzureDocumentDbStorage(this IGlobalConfiguration configuration, string url, string authSecret, string database, string collection, AzureDocumentDbStorageOptions options)
         {
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
             if (string.IsNullOrEmpty(url)) throw new ArgumentNullException(nameof(url));
             if (string.IsNullOrEmpty(authSecret)) throw new ArgumentNullException(nameof(authSecret));
             if (options == null) throw new ArgumentNullException(nameof(options));
 
-            AzureDocumentDbStorage storage = new AzureDocumentDbStorage(url, authSecret, database, options);
+            AzureDocumentDbStorage storage = new AzureDocumentDbStorage(url, authSecret, database, collection, options);
             return configuration.UseStorage(storage);
         }
     }
