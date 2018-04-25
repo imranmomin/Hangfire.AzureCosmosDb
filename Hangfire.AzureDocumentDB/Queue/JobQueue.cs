f9ee441344c0e526767c8edcb6f8bdaf5caa34b0
imranmomin <imran.amomin@outlook.com>
2018-04-25 13:08:37 -0400

    timeout on the fetched queue job

--- Hangfire.AzureDocumentDB/Queue/JobQueue.cs
+++ Hangfire.AzureDocumentDB/Queue/JobQueue.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
+using Hangfire.Azure.Documents.Helper;
 using Hangfire.Storage;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
@@ -15,6 +15,7 @@ namespace Hangfire.Azure.Queue
         private readonly DocumentDbStorage storage;
         private const string DISTRIBUTED_LOCK_KEY = "locks:job:dequeue";
         private readonly TimeSpan defaultLockTimeout = TimeSpan.FromSeconds(10);
+        private readonly TimeSpan invisibilityTimeout = TimeSpan.FromMinutes(30);
         private readonly object syncLock = new object();
 
         public JobQueue(DocumentDbStorage storage) => this.storage = storage;
@@ -30,14 +31,17 @@ namespace Hangfire.Azure.Queue
                     using (new DocumentDbDistributedLock(DISTRIBUTED_LOCK_KEY, defaultLockTimeout, storage))
                     {
                         string queue = queues.ElementAt(index);
+                        int invisibilityTimeoutEpoch = DateTime.UtcNow.Add(invisibilityTimeout.Negate()).ToEpoch();
 
                         SqlQuerySpec sql = new SqlQuerySpec
                         {
-                            QueryText = "SELECT TOP 1 * FROM doc WHERE doc.type = @type AND doc.name = @name AND NOT is_defined(doc.fetched_at)",
+                            QueryText = "SELECT TOP 1 * FROM doc WHERE doc.type = @type AND doc.name = @name AND " +
+                                        "((NOT is_defined(doc.fetched_at)) OR (is_defined(doc.fetched_at) AND doc.fetched_at < @timeout))",
                             Parameters = new SqlParameterCollection
                             {
                                 new SqlParameter("@type", Documents.DocumentTypes.Queue),
-                                new SqlParameter("@name", queue)
+                                new SqlParameter("@name", queue),
+                                new SqlParameter("@timeout", invisibilityTimeoutEpoch)
                             }
                         };
 
@@ -45,7 +49,7 @@ namespace Hangfire.Azure.Queue
                             .AsEnumerable()
                             .FirstOrDefault();
 
-                        if (data != null)
+                        if (data != null && !string.IsNullOrEmpty(data.JobId))
                         {
                             // mark the document
                             data.FetchedAt = DateTime.UtcNow;
