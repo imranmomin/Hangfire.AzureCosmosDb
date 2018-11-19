473862e2f9bb07cfd03a74ca56c231dceec32b96
imranmomin <imran.amomin@outlook.com>
2018-11-19 10:21:01 -0500

    release lock before going on for waithandle

--- Hangfire.AzureDocumentDB/ExpirationManager.cs
+++ Hangfire.AzureDocumentDB/ExpirationManager.cs
@@ -15,28 +15,30 @@ namespace Hangfire.Azure
     internal class ExpirationManager : IServerComponent
 #pragma warning restore 618
     {
-        private static readonly ILog logger = LogProvider.For<ExpirationManager>();
-        private const string DISTRIBUTED_LOCK_KEY = "expirationmanager";
-        private static readonly TimeSpan defaultLockTimeout = TimeSpan.FromMinutes(5);
-        private static readonly DocumentTypes[] documents = { DocumentTypes.Lock, DocumentTypes.Job, DocumentTypes.List, DocumentTypes.Set, DocumentTypes.Hash, DocumentTypes.Counter };
+        private readonly ILog logger = LogProvider.For<ExpirationManager>();
+        private const string DISTRIBUTED_LOCK_KEY = "locks:expiration:manager";
+        private readonly TimeSpan defaultLockTimeout;
+        private readonly DocumentTypes[] documents = { DocumentTypes.Lock, DocumentTypes.Job, DocumentTypes.List, DocumentTypes.Set, DocumentTypes.Hash, DocumentTypes.Counter };
         private readonly DocumentDbStorage storage;
         private readonly Uri spDeleteExpiredDocumentsUri;
 
         public ExpirationManager(DocumentDbStorage storage)
         {
             this.storage = storage ?? throw new ArgumentNullException(nameof(storage));
+            defaultLockTimeout = TimeSpan.FromSeconds(15) + storage.Options.ExpirationCheckInterval;
             spDeleteExpiredDocumentsUri = UriFactory.CreateStoredProcedureUri(storage.Options.DatabaseName, storage.Options.CollectionName, "deleteExpiredDocuments");
         }
 
         public void Execute(CancellationToken cancellationToken)
         {
-            foreach (DocumentTypes type in documents)
+            using (new DocumentDbDistributedLock(DISTRIBUTED_LOCK_KEY, defaultLockTimeout, storage))
             {
-                logger.Debug($"Removing outdated records from the '{type}' document.");
                 int expireOn = DateTime.UtcNow.ToEpoch();
-
-                using (new DocumentDbDistributedLock(DISTRIBUTED_LOCK_KEY, defaultLockTimeout, storage))
+                
+                foreach (DocumentTypes type in documents)
                 {
+                    logger.Trace($"Removing outdated records from the '{type}' document.");
+                    
                     int deleted = 0;
                     ProcedureResponse response;
                     do
@@ -52,9 +54,9 @@ namespace Hangfire.Azure
 
                     logger.Trace($"Outdated {deleted} records removed from the '{type}' document.");
                 }
-
-                cancellationToken.WaitHandle.WaitOne(storage.Options.ExpirationCheckInterval);
             }
+
+            cancellationToken.WaitHandle.WaitOne(storage.Options.ExpirationCheckInterval);
         }
     }
 }
