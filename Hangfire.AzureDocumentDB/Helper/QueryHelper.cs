562fbc447f9203134941d9ef8f0c1a02a9e233d7
imranmomin <imran.amomin@outlook.com>
2018-12-09 19:14:04 -0500

    added helper for query documents with continuation token

---
+++ Hangfire.AzureDocumentDB/Helper/QueryHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.Azure.Documents.Linq;
+using Microsoft.Azure.Documents.Client;
+
+namespace Hangfire.Azure.Helper
+{
+    public static class QueryHelper
+    {
+        public static List<T> ToQueryResult<T>(this IQueryable<T> source)
+        {
+            IDocumentQuery<T> query = source.AsDocumentQuery();
+            List<T> results = new List<T>();
+
+            while (query.HasMoreResults)
+            {
+                Task<FeedResponse<T>> task = query.ExecuteNextAsync<T>();
+                task.Wait();
+                results.AddRange(task.Result);
+            }
+
+            return results;
+        }
+    }
+}
