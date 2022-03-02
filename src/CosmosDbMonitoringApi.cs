88ac0d84807665f99a72443417d23b7cf0a2b2ad
Imran Momin <imran.amomin@outlook.com>
2022-03-02 16:41:02 -0500

    cached some of the queries

--- src/CosmosDbMonitoringApi.cs
+++ src/CosmosDbMonitoringApi.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using Hangfire.Azure.Documents;
-using Hangfire.Azure.Documents.Helper;
 using Hangfire.Azure.Helper;
 using Hangfire.Azure.Queue;
 using Hangfire.Common;
@@ -23,17 +22,34 @@ internal sealed class CosmosDbMonitoringApi : IMonitoringApi
 	private static DateTime cacheUpdated;
 	private static StatisticsDto? cacheStatisticsDto;
 	private readonly object cacheLock = new();
-	private readonly string[] stateNames = new[] { EnqueuedState.StateName, FailedState.StateName, ProcessingState.StateName, ScheduledState.StateName, SucceededState.StateName, AwaitingState.StateName }.Select(x => $"'{x}'").ToArray();
 	private readonly CosmosDbStorage storage;
+	private readonly QueryDefinition stateSql;
+	private readonly QueryDefinition serverSql;
+	private readonly QueryDefinition statsSql;
+	private readonly QueryDefinition recurringJobSql;
 
 	public CosmosDbMonitoringApi(CosmosDbStorage storage)
 	{
 		this.storage = storage;
+
+		// state query
+		string[] stateNames = new[] { EnqueuedState.StateName, FailedState.StateName, ProcessingState.StateName, ScheduledState.StateName, SucceededState.StateName, AwaitingState.StateName }.Select(x => $"'{x}'").ToArray();
+		stateSql = new QueryDefinition($"SELECT doc.state_name AS state, COUNT(1) AS stateCount FROM doc WHERE IS_DEFINED(doc.state_name) AND doc.state_name IN ({string.Join(",", stateNames)}) GROUP BY doc.state_name");
+
+		// server query
+		serverSql = new QueryDefinition("SELECT TOP 1 VALUE COUNT(1) FROM doc");
+
+		// stats query
+		string[] keys = { "'stats:succeeded'", "'stats:deleted'" };
+		statsSql = new QueryDefinition($"SELECT doc.key, SUM(doc['value']) AS total FROM doc WHERE doc.key IN ({string.Join(",", keys)}) GROUP BY doc.key");
+
+		// recurring job query
+		recurringJobSql = new QueryDefinition("SELECT TOP 1 VALUE COUNT(1) FROM doc WHERE doc.key = @key")
+			.WithParameter("@key", "recurring-jobs");
 	}
 
 	public IList<QueueWithTopEnqueuedJobsDto> Queues()
 	{
-
 		var tuples = storage.QueueProviders
 			.Select(x => x.GetJobQueueMonitoringApi())
 			.SelectMany(x => x.GetQueues(), (monitoring, queue) => new { Monitoring = monitoring, Queue = queue })
@@ -150,10 +166,7 @@ internal sealed class CosmosDbMonitoringApi : IMonitoringApi
 			if (cacheStatisticsDto != null && cacheUpdated.Add(cacheTimeout) >= DateTime.UtcNow) return cacheStatisticsDto;
 
 			// get counts of jobs on state
-			QueryDefinition sql = new("SELECT doc.state_name AS state, COUNT(1) AS stateCount FROM doc WHERE IS_DEFINED(doc.state_name) " +
-			                          $"AND doc.state_name IN ({string.Join(",", stateNames)}) GROUP BY doc.state_name");
-
-			List<(string state, int stateCount)> states = storage.Container.GetItemQueryIterator<JObject>(sql, requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Job })
+			List<(string state, int stateCount)> states = storage.Container.GetItemQueryIterator<JObject>(stateSql, requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Job })
 				.ToQueryResult()
 				.Select(x => (x.Value<string>("state"), x.Value<int>("stateCount")))
 				.ToList()!;
@@ -161,19 +174,14 @@ internal sealed class CosmosDbMonitoringApi : IMonitoringApi
 			Dictionary<string, long> results = states.ToDictionary<(string state, int stateCount), string, long>(state => state.state.ToLower(), state => state.stateCount);
 
 			// get counts of servers
-			sql = new QueryDefinition("SELECT TOP 1 VALUE COUNT(1) FROM doc");
-
-			long servers = storage.Container.GetItemQueryIterator<long>(sql, requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Server })
+			long servers = storage.Container.GetItemQueryIterator<long>(serverSql, requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Server })
 				.ToQueryResult()
 				.FirstOrDefault();
 
 			results.Add("servers", servers);
 
 			// get sum of stats:succeeded / stats:deleted counters
-			string[] keys = { "'stats:succeeded'", "'stats:deleted'" };
-			sql = new QueryDefinition($"SELECT doc.key, SUM(doc['value']) AS total FROM doc WHERE doc.key IN ({string.Join(",", keys)}) GROUP BY doc.key");
-
-			List<(string key, int total)> counters = storage.Container.GetItemQueryIterator<JObject>(sql, requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Counter })
+			List<(string key, int total)> counters = storage.Container.GetItemQueryIterator<JObject>(statsSql, requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Counter })
 				.ToQueryResult()
 				.Select(x => (x.Value<string>("key"), x.Value<int>("total")))
 				.ToList()!;
@@ -183,10 +191,8 @@ internal sealed class CosmosDbMonitoringApi : IMonitoringApi
 				results.Add(key, total);
 			}
 
-			sql = new QueryDefinition("SELECT TOP 1 VALUE COUNT(1) FROM doc WHERE doc.key = @key")
-				.WithParameter("@key", "recurring-jobs");
-
-			long jobs = storage.Container.GetItemQueryIterator<long>(sql, requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Set })
+			// get the recurring job counts
+			long jobs = storage.Container.GetItemQueryIterator<long>(recurringJobSql, requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Set })
 				.ToQueryResult()
 				.FirstOrDefault();
 
@@ -298,20 +304,34 @@ internal sealed class CosmosDbMonitoringApi : IMonitoringApi
 			.ToQueryResult()
 			.ToList();
 
-		string[] ids = filterJobs.Select(x => x.StateId).ToArray();
+		if (filterJobs.Count == 0) return new JobList<T>(jobs);
+
+		string[] filterJobStateIds = filterJobs.Select(x => x.StateId).ToArray();
 		List<State> states = storage.Container.GetItemLinqQueryable<State>(requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.State })
-			.Where(x => ids.Contains(x.Id))
+			.Where(x => filterJobStateIds.Contains(x.Id))
 			.ToQueryResult()
 			.ToList();
 
-		filterJobs.ForEach(job =>
+		filterJobs.ForEach(x =>
 		{
-			State state = states.Find(x => x.Id == job.StateId);
-			InvocationData invocationData = job.InvocationData;
-			invocationData.Arguments = job.Arguments;
+			T data = default!;
 
-			T data = selector(state, invocationData.DeserializeJob());
-			jobs.Add(new KeyValuePair<string, T>(job.Id, data));
+			try
+			{
+				State state = states.Find(s => s.Id == x.StateId);
+
+				InvocationData invocationData = x.InvocationData;
+				invocationData.Arguments = x.Arguments;
+				Job? job = invocationData.DeserializeJob();
+
+				data = selector(state, job);
+			}
+			catch (JobLoadException)
+			{
+				/* Ignore */
+			}
+
+			jobs.Add(new KeyValuePair<string, T>(x.Id, data));
 		});
 
 		return new JobList<T>(jobs);
@@ -329,29 +349,42 @@ internal sealed class CosmosDbMonitoringApi : IMonitoringApi
 			.ToQueryResult()
 			.ToList();
 
+		if (queues.Count == 0) return new JobList<T>(jobs);
+
 		string[] queueJobIds = queues.Select(x => x.JobId).ToArray();
 		List<Documents.Job> filterJobs = storage.Container.GetItemLinqQueryable<Documents.Job>(requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Job })
 			.Where(x => queueJobIds.Contains(x.Id))
 			.ToQueryResult()
 			.ToList();
 
-		string[] filteredJobIds = filterJobs.Select(x => x.StateId).ToArray();
+		string[] filteredJobStateIds = filterJobs.Select(x => x.StateId).ToArray();
 		List<State> states = storage.Container.GetItemLinqQueryable<State>(requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.State })
-			.Where(x => filteredJobIds.Contains(x.Id))
+			.Where(x => filteredJobStateIds.Contains(x.Id))
 			.ToQueryResult()
 			.ToList();
 
-		queues.ForEach(queueItem =>
+		foreach (Documents.Queue queueItem in queues)
 		{
-			Documents.Job job = filterJobs.Find(x => x.Id == queueItem.JobId);
-			InvocationData invocationData = job.InvocationData;
-			invocationData.Arguments = job.Arguments;
+			T data = default!;
 
-			State? state = states.SingleOrDefault(x => x.Id == job.StateId);
+			try
+			{
+				Documents.Job sqlJob = filterJobs.Find(x => x.Id == queueItem.JobId);
+				State? state = states.SingleOrDefault(x => x.Id == sqlJob.StateId);
 
-			T data = selector(state, invocationData.DeserializeJob(), queueItem.FetchedAt);
-			jobs.Add(new KeyValuePair<string, T>(job.Id, data));
-		});
+				InvocationData invocationData = sqlJob.InvocationData;
+				invocationData.Arguments = sqlJob.Arguments;
+				Job? job = invocationData.DeserializeJob();
+
+				data = selector(state, job, queueItem.FetchedAt);
+			}
+			catch (JobLoadException)
+			{
+				/* Ignore */
+			}
+
+			jobs.Add(new KeyValuePair<string, T>(queueItem.JobId, data));
+		}
 
 		return new JobList<T>(jobs);
 	}
@@ -412,7 +445,7 @@ internal sealed class CosmosDbMonitoringApi : IMonitoringApi
 	{
 		DateTime endDate = DateTime.UtcNow;
 		List<DateTime> dates = new();
-		for (int i = 0; i < 24; i++)
+		for (int i = 0; i < 20; i++)
 		{
 			dates.Add(endDate);
 			endDate = endDate.AddHours(-1);
@@ -441,7 +474,7 @@ internal sealed class CosmosDbMonitoringApi : IMonitoringApi
 		Dictionary<DateTime, long> result = keys.ToDictionary(k => k.Value, _ => default(long));
 		string[] filter = keys.Keys.Select(x => $"'{x}'").ToArray();
 
-		QueryDefinition sql = new($"SELECT doc.key, SUM(doc['value']) AS total FROM doc WHERE doc.key IN ({string.Join(",", filter)}) GROUP BY doc.key");
+		QueryDefinition sql = new($"SELECT doc.key, SUM(doc['value']) AS total FROM doc WHERE doc.counterType = {(int)CounterTypes.Aggregate} AND doc.key IN ({string.Join(",", filter)}) GROUP BY doc.key");
 
 		List<(string key, int total)> data = storage.Container.GetItemQueryIterator<JObject>(sql, requestOptions: new QueryRequestOptions { PartitionKey = PartitionKeys.Counter })
 			.ToQueryResult()
@@ -450,7 +483,7 @@ internal sealed class CosmosDbMonitoringApi : IMonitoringApi
 
 		foreach (string key in keys.Keys)
 		{
-			DateTime date = keys.Where(k => k.Key == key).Select(k => k.Value).First();
+			DateTime date = keys.Where(k => k.Key == key).Select(k => k.Value).Single();
 			(string key, int total) item = data.SingleOrDefault(x => x.key == key);
 			result[date] = item.total;
 		}
